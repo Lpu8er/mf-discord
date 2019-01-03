2b9ce01d5a2ee2a259fc4ba402dbcea99cc47bd9
Lpu8er <lpu8er@gmail.com>
2019-01-03 12:46:02 +0100

    With the right generator, this is better

--- src/Utils/IdGenerators/TagIdGenerator.php
+++ src/Utils/IdGenerators/TagIdGenerator.php
@@ -12,14 +12,20 @@ use Doctrine\ORM\Id\AbstractIdGenerator;
  * @author lpu8er
  */
 class TagIdGenerator extends AbstractIdGenerator {
-    
+
     /**
      * 
      * @param EntityManager $em
      * @param Tag $entity
      */
     public function generate(EntityManager $em, $entity) {
-        return 'a';
-        //return strtolower($entity);
+        return strtolower( // lower it
+                preg_replace('`(\W+)`', '-', // remove non-compatible chars
+                        iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', // transform accents
+                                preg_replace('`[:cntrl:]`', '', $entity->getLabel()) // remove ctrl chars
+                        )
+                )
+        );
     }
+
 }
