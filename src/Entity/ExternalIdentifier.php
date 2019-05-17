ad39e2df1998459f722f7ae02a8c63e4ea550372
Lpu8er <lpu8er@gmail.com>
2019-05-17 11:48:27 +0200

    Add auth

--- src/Entity/ExternalIdentifier.php
+++ src/Entity/ExternalIdentifier.php
@@ -39,4 +39,38 @@ class ExternalIdentifier {
      * @ORM\Column(type="string", nullable=true)
      */
     private $mcuid = null;
+
+    public function getSyskey(): ?string
+    {
+        return $this->syskey;
+    }
+
+    public function getSysval(): ?string
+    {
+        return $this->sysval;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getMcuid(): ?string
+    {
+        return $this->mcuid;
+    }
+
+    public function setMcuid(?string $mcuid): self
+    {
+        $this->mcuid = $mcuid;
+
+        return $this;
+    }
 }
