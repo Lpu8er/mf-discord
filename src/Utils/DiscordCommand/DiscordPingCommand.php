00b3fb6e032abda532f16870d2d7c0e749240912
Lpu8er <lpu8er@gmail.com>
2020-06-17 14:37:28 +0200

    Add embeds

--- src/Utils/DiscordCommand/DiscordPingCommand.php
+++ src/Utils/DiscordCommand/DiscordPingCommand.php
@@ -10,6 +10,6 @@ use App\Service\Discord;
  */
 class DiscordPingCommand extends DiscordAdmin {
     public function execute(Discord $discordService) {
-        $discordService->talk($this->data['message'], $this->data['channel']);
+        $discordService->talk($this->data['message'], $this->data['channel'], empty($this->data['embed'])? []:$this->data['embed']);
     }
 }
